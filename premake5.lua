812fbd95117a82e24fe44c332887bd4f23d2dd85
luizfelipemb <>
2024-05-03 21:40:36 +0100

    basic sdl c++ game with premake5

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+workspace "OresSDL"
+    platforms { "Windows" }
+    configurations { "Debug", "Release" }
+    flags "MultiProcessorCompile"
+
+project "OresSDL"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    
+    targetdir "bin/%{cfg.system}-%{cfg.buildcfg}/%{cfg.architecture}"
+    objdir "bin-int/%{cfg.system}-%{cfg.buildcfg}/%{cfg.architecture}"
+
+    files { "src/**.cpp", "include/**.h", }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+    
+
+    filter "platforms:Windows"
+        system "Windows"
+        architecture "x86_64"
+
+
+    filter "system:windows"
+        system "Windows"
+
+        includedirs { "SDL2-2.30.3/include" }
+        libdirs { "SDL2-2.30.3/lib/x64" }
+
+        links { "SDL2main", "SDL2" }
+		
+ -- Post-build action to copy SDL2.dll
+        postbuildcommands {
+            "{COPY} %{path.getabsolute('SDL2-2.30.3/lib/x64/SDL2.dll')} %{cfg.targetdir}"
+        }
